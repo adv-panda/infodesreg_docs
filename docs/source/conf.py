bc6d1e1c01880f340a4c4328fd258907b9c5ac6d
adv-panda <98755510+adv-panda@users.noreply.github.com>
2024-05-12 09:01:24 +0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'InfoDESReg'
+copyright = '2024, Anonymous'
+author = 'Anonymous'
 
 release = '0.1'
 version = '0.1.0'
